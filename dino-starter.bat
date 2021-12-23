ff855b6e1d59836a36397e39a760dfc56a68498d
natrix_dev <discordlenko@gmail.com>
2021-12-23 12:48:13 +0100

    Create dino-starter.bat

---
+++ dino-starter.bat
@@ -0,0 +1,13 @@
+color a 
+npm init 
+cls
+color c 
+npm i discord.js, dino @latest --save
+cls
+color d 
+node dino-code.js
+cls
+color a 
+@echo off
+Started ! - $staus : woring - $servers : servers
+
